7062d49424dfd890a5be8df3399e93bd43dd9e85
Jona Joy <jonajoy142@gmail.com>
2023-02-01 18:35:12 +0530

    initial commit!

---
+++ lib/screens/taskScreen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class TaskScreen extends StatelessWidget {
+  const TaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                color: Colors.yellow,
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(40))),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, top: 50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const Icon(
+                    FontAwesomeIcons.barsStaggered,
+                  ),
+                  SizedBox(
+                    height: 10,
+                    width: double.infinity,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 120, bottom: 10),
+                    child: Text(
+                      "TASK",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      
+    );
+  }
+}
